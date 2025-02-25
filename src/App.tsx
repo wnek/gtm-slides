dfcc160412a98a360f358ab542d6df20bcb090e7
Mariusz Wnęk <wnek.mariusz@gmail.com>
2025-02-25 23:09:07 +0100

    Add cards animation, add lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,74 @@
-import { useRef } from 'react'
+import { useRef, forwardRef, useEffect } from 'react'
 import { motion, useScroll, useTransform } from "framer-motion"
 
-interface CardProps {
-  y: any
-  scale: any
-  id: number
-  title: string
-  text: string
-  image: string
-}
 
-function Card({ y, scale, id, title, text, image }: CardProps) {
-  return (
-    <motion.li
-      style={{
-        y,
-        scale,
-        paddingTop: `calc(${id} * 1em)`, // --card-top-offset equivalent
-      }}
-      className='sticky top-0 origin-top will-change-transform'
-    >
-      <div className='bg-white text-[rgb(10,5,7)] rounded-2xl overflow-hidden shadow-[0_0.2em_1em_rgba(0,0,0,0.1),_0_1em_2em_rgba(0,0,0,0.1)] grid grid-cols-2'>
-        <div className='w-4/5 place-self-center text-left grid gap-4 place-items-start'>
-          <h2 className='text-4xl font-light'>{title}</h2>
-          <p className='font-light leading-relaxed'>{text}</p>
-          <p>
-            <a href="#top" className='inline-block bg-[rgb(188,87,36)] text-white no-underline px-2 py-2 rounded'>
-              Read more
-            </a>
-          </p>
-        </div>
-        <figure className='overflow-hidden'>
-          <img src={image} alt={title} className='w-full h-full object-cover' />
-        </figure>
-      </div>
-    </motion.li>
-  )
-}
 
 function App() {
   const containerRef = useRef(null)
+
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ['start start', 'end start']
+    offset: ["start start", "end end"]
   })
 
-  const totalCards = 4
-  const cards = [
-    {
-      title: "Card One",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "https://codyhouse.co/demo-tutorials/stacking-cards/assets/img/img-1.jpg"
-    },
-    {
-      title: "Card Two",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "https://codyhouse.co/demo-tutorials/stacking-cards/assets/img/img-2.jpg"
-    },
-    {
-      title: "Card Three",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "https://codyhouse.co/demo-tutorials/stacking-cards/assets/img/img-3.jpg"
-    },
-    {
-      title: "Card Four",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "https://codyhouse.co/demo-tutorials/stacking-cards/assets/img/img-2.jpg"
-    }
-  ]
+  // useEffect(() => {
+  //   scrollYProgress.on('change', (latest) => {
+  //     console.log(latest)
+  //   })
+  // })
 
-  const slides = cards.map((card, i) => {
-    const index = i + 1
-    const reverseIndex0 = totalCards - (i + 1)
+  const scale1 = useTransform(scrollYProgress, [0, 1], [1, 0.9])
+  const scale2 = useTransform(scrollYProgress, [0.33, 1], [1, 0.9])
 
-    const start = i / totalCards
-    const end = index / totalCards
+  const blur1 = useTransform(scrollYProgress, [0, 1], [0, 20])
 
-    return {
-      ...card,
-      y: useTransform(scrollYProgress, [start, end], ['0%', '-100%']),
-      scale: useTransform(scrollYProgress, [start, end], [1, 1 - (0.1 * reverseIndex0)]),
-      id: index
-    }
-  })
 
   return (
-    <div className='bg-[rgb(58,29,43)] text-white text-center text-[calc(1em+0.5vw)]'>
-      <header className='w-[80vw] h-screen mx-auto grid place-items-center'>
-        <div>
-          <h1 className='text-6xl font-light'>Stacking Cards</h1>
-          <p>👇 Scroll down to see the effect.</p>
+    <section>
+
+      <div className='h-screen bg-pink-500'>
+        <h1>Hello</h1>
+      </div>
+
+      <div className="border-2 border-black" ref={containerRef}>
+        <div className="h-screen sticky top-0 flex justify-center items-center" >
+          <motion.div
+            className="h-3/4 w-3/4 bg-red-500"
+            style={{
+              scale: scale1,
+              filter: `blur(${blur1}px)`
+            }}
+          >
+            <h1>Slide1</h1>
+          </motion.div>
         </div>
-      </header>
 
-      <main className='w-[80vw] mx-auto'>
-        <ul
-          ref={containerRef}
-          className='list-none grid grid-cols-1 gap-[4vw] pb-[calc(4*1em)]'
-          style={{
-            gridTemplateRows: `repeat(${totalCards}, 40vw)`,
-          }}
-        >
-          {slides.map((slide) => (
-            <Card key={slide.id} {...slide} />
-          ))}
-        </ul>
-      </main>
+        <div className="h-screen sticky top-0 flex justify-center items-center" >
+          <motion.div
+            className="h-3/4 w-3/4 bg-green-500"
+            style={{
+              scale: scale2,
+              filter: `blur(10px)`,
 
-      <aside className='w-1/2 mx-auto text-left'>
-        <p className='mb-4'>Lorem ipsum dolor sit amet consectetur...</p>
-        {/* Add more paragraphs as needed */}
-      </aside>
-    </div>
+            }}
+          />
+        </div>
+
+        <div className="h-screen sticky top-0 flex justify-center items-center" >
+          <motion.div className="h-3/4 w-3/4 bg-yellow-500" />
+        </div>
+
+
+
+
+      </div>
+
+      <div className='h-screen bg-pink-500'>
+        <h1>Hello</h1>
+      </div>
+
+    </section>
+
   )
 }
 
