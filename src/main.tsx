80607f34740f958c793721c20ffc003d3cd507c9
Mariusz <wnek.mariusz@gmail.com>
2025-03-04 12:15:31 +0100

    Fix error in the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis } from 'lenis/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
